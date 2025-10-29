304ee1f3fb1ce6c2f9f4769de4852eaffb0de061
LEI-122718 <cfssa6@iscte-iul.pt>
2025-10-29 11:18:01 +0000

    V3

--- src/v0/Rental.java
+++ src/v0/Rental.java
@@ -1,11 +1,13 @@
 package v0;
 
+import v3.Movie;
+
 public class Rental
 {
-    private Movie	_movie;
+    private v3.Movie _movie;
     private int		_daysRented;
 
-    public Rental(Movie movie, int daysRented)
+    public Rental(v3.Movie movie, int daysRented)
     {
         _movie = movie;
         _daysRented = daysRented;
