000d872412ad224f5775c21fe24c3b59cae00e73
LEI-122695 <matildegit5@gmail.com>
2025-10-29 11:14:36 +0000

    V2

--- src/v0/Movie.java
+++ src/v0/Movie.java
@@ -2,21 +2,21 @@ package v0;
 
 public class Movie {
 
-	public enum Code {REGULAR, CHILDRENS, NEW_RELEASE};
-	
-	private String _title;
-	private Code _priceCode;
-	
-	public Movie(String title, Code priceCode) {
-		_title = title;
-		_priceCode = priceCode;
-	}
+    public enum Code {REGULAR, CHILDRENS, NEW_RELEASE};
 
-	public String getTitle() {
-		return _title;
-	}
+    private String _title;
+    private Code _priceCode;
 
-	public Code getPriceCode() {
-		return _priceCode;
-	}
+    public Movie(String title, Code priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
+
+    public String getTitle() {
+        return _title;
+    }
+
+    public Code getPriceCode() {
+        return _priceCode;
+    }
 }
