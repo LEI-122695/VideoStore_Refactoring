461d9e9d9a55994c81fdca98ff8265b22bbff524
LEI-122662 <rrlfx@iscte-iul.pt>
2025-10-29 11:25:39 +0000

    V3-->V4 changes

--- src/v0/Customer.java
+++ src/v0/Customer.java
@@ -1,22 +1,18 @@
 package v0;
 
-
-import v3.Movie;
-import v3.Rental;
-
 import java.util.Vector;
 
 public class Customer
 {
     private String			_name;
-    private Vector<v3.Rental>	_rentals	= new Vector<v3.Rental>();
+    private Vector<Rental>	_rentals	= new Vector<Rental>();
 
     public Customer(String _name)
     {
         this._name = _name;
     }
 
-    public void addRental(v3.Rental arg)
+    public void addRental(Rental arg)
     {
         _rentals.addElement(arg);
     }
@@ -34,9 +30,9 @@ public class Customer
         // header
         String result = "Rental Record for " + getName() + "\n";
 
-        for (v3.Rental each: _rentals)
+        for (Rental each: _rentals)
         {
-            frequentRenterPoints += getFrequentRentalPoints(each);
+            frequentRenterPoints += each.getFrequentRentalPoints();
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
@@ -47,10 +43,5 @@ public class Customer
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
         return result;
-    }
-
-    public int getFrequentRentalPoints(Rental each)
-    {
-        return ((each.getMovie().getPriceCode() == Movie.Code.NEW_RELEASE) && each.getDaysRented() > 1) ? 2 : 1;
     }
 }
